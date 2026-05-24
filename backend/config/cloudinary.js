7ba042fc5199c0d7d43df7dfdd3144aec8b55424
Mdezaz628 <ansarimdezaz01@gmail.com>
2026-05-24 19:08:57 +0530

    fixed cloudinary env names

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -3,9 +3,9 @@ import cloudinaryPkg from "cloudinary";
 const cloudinary = cloudinaryPkg.v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 export default cloudinary;
